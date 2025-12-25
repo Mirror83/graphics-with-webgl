ca687b95d146b68a6da4076a06f846f4cd9ce01d
Mirror83 <glenochieng045@gmail.com>
2025-12-25 16:49:12 +0300

    fix: change background colour of page to use default clear colour
    this allows transparent textures to look transparent against
    the canvas; without this, the transparent bits were coloured
    by the underlying page's colour.

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -9,4 +9,20 @@ export const Colors = {
   darkBluishGreen: { r: 0.2, g: 0.3, b: 0.3, a: 1.0 }
 };
 
+const MAX_COLOUR_VALUE = 255;
+
+function componentToHex(c: number): string {
+  return Math.floor(c * MAX_COLOUR_VALUE)
+    .toString(16)
+    .padStart(2, "0");
+}
+
+export function toHexColourString(color: Color): string {
+  const r = componentToHex(color.r);
+  const g = componentToHex(color.g);
+  const b = componentToHex(color.b);
+  const a = componentToHex(color.a);
+  return `#${r}${g}${b}${a}`;
+}
+
 export const defaultClearColor = Colors.darkBluishGreen;
--- src/routes/scenes/[sceneName]/+page.svelte
+++ src/routes/scenes/[sceneName]/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+  import { defaultClearColor, toHexColourString } from "~/lib/color";
   import type { PageData } from "./$types";
   const { data }: { data: PageData } = $props();
+  const bgColour = toHexColourString(defaultClearColor);
 </script>
 
-<canvas {@attach data.renderWrapper}>{data.description}</canvas>
+<main class={`bg-[${bgColour}] text-white`}>
+  <canvas {@attach data.renderWrapper}>{data.description}</canvas>
+</main>
