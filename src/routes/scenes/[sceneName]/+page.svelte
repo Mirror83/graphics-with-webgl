53a629ab42df7bfb7ce4c7c349341e49363fc43a
Mirror83 <glenochieng045@gmail.com>
2025-12-26 10:48:22 +0300

    feat: add dialog to show info for current scene

--- src/routes/scenes/[sceneName]/+page.svelte
+++ src/routes/scenes/[sceneName]/+page.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
   import { defaultClearColor, toHexColourString } from "~/lib/color";
   import type { PageData } from "./$types";
+  import { Info } from "@lucide/svelte";
+  let dialog: HTMLDialogElement;
+
+  function showDialog() {
+    dialog.showModal();
+  }
+
+  function closeDialog() {
+    dialog.close();
+  }
+
   const { data }: { data: PageData } = $props();
   const bgColour = toHexColourString(defaultClearColor);
 </script>
@@ -9,4 +20,23 @@
   <canvas tabindex="0" class="focus:outline-0" {@attach data.renderWrapper}
     >{data.description}</canvas
   >
+
+  <dialog
+    class="m-auto bg-transparent px-4 backdrop:backdrop-blur"
+    closedby="any"
+    bind:this={dialog}
+  >
+    <div class="max-w-[80ch] bg-blue-100 p-8 text-blue-900 dark:bg-blue-900 dark:text-blue-100">
+      <h1 class="text-2xl font-bold">{data.name}</h1>
+      <p>{data.description}</p>
+      {#if data.inputInstructions}
+        <p class="mt-8">{data.inputInstructions}</p>
+      {/if}
+      <button class="mt-8 border-2 p-2" onclick={closeDialog}>Close</button>
+    </div>
+  </dialog>
+
+  <div class="absolute bottom-8 left-8">
+    <button aria-label="Open Info" onclick={showDialog}><Info /></button>
+  </div>
 </main>
