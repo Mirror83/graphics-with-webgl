445987dd7a3ddacc0c09e9a892d230590b58907b
Mirror83 <glenochieng045@gmail.com>
2025-12-26 21:23:32 +0300

    feat: add a short description at root route page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<main class="flex min-h-screen items-center justify-center">Graphics with WebGL</main>
+<main class="flex min-h-screen flex-col items-center justify-center">
+  <h1 class="text-3xl font-bold">Graphics with WebGL</h1>
+  <p>
+    Implementations of the content in <a
+      class="underline"
+      href="https://learnopengl.com"
+      target="_blank">Learn OpenGL</a
+    > using Javascript + WebGL
+  </p>
+</main>
