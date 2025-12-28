51ba8c7c8f8b1330a39b909fc3abd0f1b998c5c5
Mirror83 <glenochieng045@gmail.com>
2025-12-28 09:22:31 +0300

    chore: adjust styles and content for index route
    a few small modifications to make the content look better

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
-<main class="flex min-h-screen flex-col items-center justify-center">
-  <h1 class="text-3xl font-bold">Graphics with WebGL</h1>
+<main class="flex min-h-screen flex-col items-center justify-center px-4 text-center">
+  <h1 class="mb-4 text-3xl font-bold">Graphics with WebGL</h1>
   <p>
-    Implementations of the content in <a
+    Implementations of the demos in <a
       class="underline"
       href="https://learnopengl.com"
       target="_blank">Learn OpenGL</a
