b07a3664fb2f3217ff26b48f14ca9c99c8be0f2e
Mirror83 <glenochieng045@gmail.com>
2026-01-05 22:50:55 +0300

    fix: enable SSG for the whole application

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,9 @@
-// Tauri doesn't have a Node.js server to do proper SSR
-// so we use adapter-static with a fallback to index.html to put the site in SPA mode
+// Tauri doesn't have a Node.js server to do proper SSR,
+// so SSR is disabled, and the app is pre-rendered as a static site.
+// This comes with the limitation that Tauri APIs cannot be used in
+// `load` functions, but as this app only uses Tauri to render the
+// SvelteKit app in a desktop window (for now), this is acceptable.
 // See: https://svelte.dev/docs/kit/single-page-apps
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
 export const ssr = false;
+export const prerender = true;
