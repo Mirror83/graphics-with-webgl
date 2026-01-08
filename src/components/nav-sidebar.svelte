fc1f869ccd27f56dce554df0435741104abed77e
Mirror83 <glenochieng045@gmail.com>
2026-01-08 11:43:33 +0300

    fix: make sidebar scrollable

--- src/components/nav-sidebar.svelte
+++ src/components/nav-sidebar.svelte
@@ -9,14 +9,17 @@
 
   const isPageError = $derived(page.error);
 
+  let sidebar: HTMLDialogElement;
   let sidebarOpen = $state(false);
 
   function openSidebar() {
     sidebarOpen = true;
+    sidebar.showModal();
   }
 
   function closeSidebar() {
     sidebarOpen = false;
+    sidebar.close();
   }
 </script>
 
@@ -38,31 +41,31 @@
   </button>
 {/if}
 
-{#if sidebarOpen}
+<dialog
+  class="relative min-h-screen min-w-screen bg-transparent backdrop:backdrop-blur"
+  bind:this={sidebar}
+>
   <div
-    class="absolute top-0 right-0 z-20 min-h-screen min-w-screen backdrop-blur"
-    role="presentation"
+    class="absolute top-0 right-0 max-h-screen w-xs overflow-auto bg-blue-200 px-4 py-4 dark:bg-blue-800"
   >
-    <div class="absolute top-0 right-0 z-30 min-h-screen w-xs bg-blue-200 px-4 dark:bg-blue-800">
-      <button class="absolute top-8 right-8" aria-label={"Close sidebar"} onclick={closeSidebar}>
-        <X />
-      </button>
-      <nav class="relative top-16">
-        <a href="/" class="text-xl underline underline-offset-8" onclick={closeSidebar}
-          >Graphics with WebGL</a
-        >
-        <ul class="mt-4 list-disc ps-4">
-          {#each sceneDetailsList as { route, name }}
-            <li>
-              <a
-                href={`/scenes/${route}`}
-                onclick={closeSidebar}
-                class={["underline", currentPath === route && "font-bold"]}>{name}</a
-              >
-            </li>
-          {/each}
-        </ul>
-      </nav>
-    </div>
+    <button class="absolute top-8 right-8" aria-label={"Close sidebar"} onclick={closeSidebar}>
+      <X />
+    </button>
+    <nav class="mt-14">
+      <a href="/" class="text-xl underline underline-offset-8" onclick={closeSidebar}
+        >Graphics with WebGL</a
+      >
+      <ul class="mt-4 list-disc overflow-auto ps-4">
+        {#each sceneDetailsList as { route, name }}
+          <li>
+            <a
+              href={`/scenes/${route}`}
+              onclick={closeSidebar}
+              class={["underline", currentPath === route && "font-bold"]}>{name}</a
+            >
+          </li>
+        {/each}
+      </ul>
+    </nav>
   </div>
-{/if}
+</dialog>
