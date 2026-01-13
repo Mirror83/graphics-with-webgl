c892906d67516ea11e1f609e9b0d139b865cbb9a
Mirror83 <glenochieng045@gmail.com>
2026-01-13 14:25:10 +0300

    fix: check deployment URL based on target environment

--- src/routes/scenes/2d-game/breakout/+page.server.ts
+++ src/routes/scenes/2d-game/breakout/+page.server.ts
@@ -1,10 +1,18 @@
-import { VERCEL_URL } from "$env/static/private";
+import { VERCEL_URL, VERCEL_TARGET_ENV, VERCEL_BRANCH_URL } from "$env/static/private";
 
 export function load() {
-  const breakoutAssetsBaseURL = VERCEL_URL
-    ? `https://${VERCEL_URL}/assets/breakout`
-    : `http://localhost:1420/assets/breakout`;
+  const baseURL = getBaseURL();
+  const breakoutAssetsBaseURL = `${baseURL}/assets/breakout`;
   return {
     breakoutAssetsBaseURL
   };
+}
+
+function getBaseURL() {
+  if (VERCEL_TARGET_ENV === "production") {
+    return `https://${VERCEL_URL}`;
+  } else if (VERCEL_TARGET_ENV === "preview") {
+    return `https://${VERCEL_BRANCH_URL}`;
+  }
+  return `http://localhost:1420`;
 }
