8b30361231aeff24dc65a01eea7773ecaf6f372b
Mirror83 <glenochieng045@gmail.com>
2026-02-01 07:11:59 +0300

    feat: update `move` to return whether the ball collided with a wall
    the "wall" refers to either the left, right or top window borders

--- src/lib/game/game-object.ts
+++ src/lib/game/game-object.ts
@@ -86,25 +86,33 @@ export class Ball extends BreakoutGameObject {
     this.radius = properties.radius ?? Ball.INITIAL_RADIUS;
   }
 
-  move(deltaTime: number, windowWidth: number, newPositionWhenStuck?: vec2): vec2 {
+  move(
+    deltaTime: number,
+    windowWidth: number,
+    newPositionWhenStuck?: vec2
+  ): { position: vec2; collidedWithWall: boolean } {
+    let collidedWithWall = false;
     if (this.stuck) {
       this.position = newPositionWhenStuck ? newPositionWhenStuck : this.position;
-      return this.position;
+      return { position: this.position, collidedWithWall };
     }
     this.position = vec2.scaleAndAdd(this.position, this.position, this.velocity, deltaTime);
 
     if (this.position[0] <= 0) {
       this.velocity[0] = -this.velocity[0];
       this.position[0] = 0;
+      collidedWithWall = true;
     } else if (this.position[0] + this.size[0] >= windowWidth) {
       this.velocity[0] = -this.velocity[0];
       this.position[0] = windowWidth - this.size[0];
+      collidedWithWall = true;
     }
     if (this.position[1] <= 0) {
       this.velocity[1] = -this.velocity[1];
       this.position[1] = 0;
+      collidedWithWall = true;
     }
-    return this.position;
+    return { position: this.position, collidedWithWall: collidedWithWall };
   }
 
   reset(
