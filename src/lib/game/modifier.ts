c109c3a8e1cf06242d666f462eb6394a042e53b2
Mirror83 <glenochieng045@gmail.com>
2026-02-06 06:29:51 +0300

    refactor: separate modifier state from the game object the paddle will interact with

--- src/lib/game/modifier.ts
+++ src/lib/game/modifier.ts
@@ -1,5 +1,8 @@
 import { vec2, vec4 } from "gl-matrix";
 import { BreakoutGameObject, type BreakoutGameObjectProperties } from "~/lib/game/game-object";
+import type { ResourceManager } from "~/lib/game/resource-manager";
+import type { SpriteRenderer } from "~/lib/game/sprite";
+import type { Texture2D } from "~/lib/textures";
 
 export const breakoutModifierNames = [
   "ball-speed-increase",
@@ -12,40 +15,45 @@ export const breakoutModifierNames = [
 
 export type BreakoutModifierName = (typeof breakoutModifierNames)[number];
 
-export type BreakoutModifierProperties = Omit<BreakoutGameObjectProperties, "size" | "colour"> & {
+export type BreakoutModifierProperties = {
   name: BreakoutModifierName;
-  colour?: vec4;
   duration?: BreakoutModifierDuration;
 };
 
 export type BreakoutModifierDuration = null | number;
 
+export const modifierSpawnPossibilities = [...breakoutModifierNames, null] as const;
+export const modifierSpawnWeights = modifierSpawnPossibilities.map((name) => {
+  switch (name) {
+    case "ball-speed-increase":
+    case "sticky-paddle":
+    case "pass-through":
+    case "paddle-size-increase":
+      return 0;
+    case "confuse":
+    case "chaos":
+      return 1;
+    case null:
+      return 0;
+    default:
+      throw new Error(`Unknown modifier name: ${name}`);
+  }
+});
+
+type BreakoutModifierPillProperties = Omit<BreakoutGameObjectProperties, "size"> & {
+  modifierName: BreakoutModifierName;
+  size?: number;
+};
+
 /**
- * Changes an aspect (or aspects) of the game either visually
- * (e.g. through post-processing effects),
- * mechanically (e.g. by speeding up the ball),
- * or a combination of both.
- * */
-export class BreakoutModifier extends BreakoutGameObject {
+ * This represents what the paddle "physically" interacts with in-game in order to activate
+ * a modifier effect.
+ */
+export class BreakoutModifierPill extends BreakoutGameObject {
   static readonly SIZE = vec2.fromValues(60.0, 20.0);
   static readonly VELOCITY = vec2.fromValues(0.0, 150.0);
 
-  name: BreakoutModifierName;
-  duration: BreakoutModifierDuration;
-  isActive: boolean = false;
-
-  constructor(properties: BreakoutModifierProperties) {
-    super({ ...properties, velocity: BreakoutModifier.VELOCITY, size: BreakoutModifier.SIZE });
-    this.name = properties.name;
-    this.colour = properties.colour ?? BreakoutModifier.getDefaultModifierColour(this.name);
-    this.duration = properties.duration ?? BreakoutModifier.getDefaultModifierDuration(this.name);
-  }
-
-  move(dt: number) {
-    vec2.scaleAndAdd(this.position, this.position, this.velocity, dt);
-  }
-
-  static getDefaultModifierColour(name: BreakoutModifierName) {
+  static getDefaultColour(name: BreakoutModifierName) {
     switch (name) {
       case "confuse":
         return vec4.fromValues(1.0, 0.3, 0.3, 1.0);
@@ -64,7 +72,44 @@ export class BreakoutModifier extends BreakoutGameObject {
     }
   }
 
-  static getDefaultModifierDuration(name: BreakoutModifierName): BreakoutModifierDuration {
+  static getDefaultSprite(modifierName: BreakoutModifierName, resourceManager: ResourceManager) {
+    const textureName = `modifier-${modifierName}` as const;
+    const texture = resourceManager.getTexture(textureName);
+    if (!texture) {
+      throw new Error(
+        `Texture with name: '${textureName}' not found for modifier: '${modifierName}'`
+      );
+    }
+    return texture;
+  }
+
+  constructor(properties: BreakoutModifierPillProperties) {
+    super({
+      ...properties,
+      velocity: BreakoutModifierPill.VELOCITY,
+      size: BreakoutModifierPill.SIZE,
+      colour: properties.colour ?? BreakoutModifierPill.getDefaultColour(properties.modifierName)
+    });
+  }
+
+  move(dt: number) {
+    vec2.scaleAndAdd(this.position, this.position, this.velocity, dt);
+  }
+}
+
+/**
+ * Changes an aspect (or aspects) of the game either visually
+ * (e.g. through post-processing effects),
+ * mechanically (e.g. by speeding up the ball),
+ * or a combination of both.
+ * */
+export class BreakoutModifier {
+  name: BreakoutModifierName;
+  duration: BreakoutModifierDuration;
+  isActive: boolean = false;
+  pill: BreakoutModifierPill | null = null;
+
+  static getDefaultDuration(name: BreakoutModifierName): BreakoutModifierDuration {
     switch (name) {
       case "confuse":
         return 15.0;
@@ -82,22 +127,26 @@ export class BreakoutModifier extends BreakoutGameObject {
         throw new Error(`Unknown modifier name: ${name}`);
     }
   }
-}
 
-export const modifierSpawnPossibilities = [...breakoutModifierNames, null] as const;
-export const modifierSpawnWeights = modifierSpawnPossibilities.map((name) => {
-  switch (name) {
-    case "ball-speed-increase":
-    case "sticky-paddle":
-    case "pass-through":
-    case "paddle-size-increase":
-      return 1; // least likely
-    case "confuse":
-    case "chaos":
-      return 3; // more likely
-    case null:
-      return 0; // most likely
-    default:
-      throw new Error(`Unknown modifier name: ${name}`);
+  constructor(properties: BreakoutModifierProperties) {
+    this.name = properties.name;
+    this.duration = properties.duration ?? BreakoutModifier.getDefaultDuration(this.name);
   }
-});
+
+  spawnModifierPill(position: vec2, sprite: Texture2D) {
+    this.pill = new BreakoutModifierPill({
+      modifierName: this.name,
+      position,
+      sprite
+    });
+  }
+
+  destroyModifierPill() {
+    this.pill = null;
+  }
+
+  drawModifierPill(gl: WebGL2RenderingContext, spriteRenderer: SpriteRenderer) {
+    if (!this.pill) return;
+    this.pill.draw(gl, spriteRenderer);
+  }
+}
