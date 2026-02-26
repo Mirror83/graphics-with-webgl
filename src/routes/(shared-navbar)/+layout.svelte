f99fdf54b546a851a2c71f12c1e7f1de24cf46ed
Mirror83 <glenochieng045@gmail.com>
2026-02-26 10:26:31 +0300

    fix: break out game from shared navbar layout

---
+++ src/routes/(shared-navbar)/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import NavSidebar from "~/components/nav-sidebar.svelte";
+  import type { LayoutProps } from "./$types";
+
+  let { children }: LayoutProps = $props();
+</script>
+
+<NavSidebar />
+{@render children()}
